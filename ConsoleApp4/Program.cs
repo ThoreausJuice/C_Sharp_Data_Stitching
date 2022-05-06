07721cccd0b65e11f61a94aac9654dbf80a9a674
Thoreausjuice <qiuguanglei@outlook.com>
2022-05-06 23:39:11 +0800

    遇到了有意思的问题

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 5, j = 5;
+            // Console.WriteLine(i += i++);
+            // Console.WriteLine(i);
+            // int result = (i += i++) > j ? i : j;
+
+            if(i++ > j)
+            {
+                Console.WriteLine(1);
+                Console.WriteLine(i);
+            }
+            else
+            {
+                Console.WriteLine(2);
+                Console.WriteLine(i);
+            }
+            // Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
